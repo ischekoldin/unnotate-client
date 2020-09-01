ff9385623c17dc749c91197e2c76ac0ce80e38c8
ischekoldin <lesterfrance@gmail.com>
2020-09-01 22:05:12 +0500

    fix formatting

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 
-import "./SideBar.css";
+import "./SideBar.scss";
 import {useDispatch} from "react-redux";
 
 import ChangePasswordForm from "../ChangePasswordForm/ChangePasswordForm";
@@ -16,26 +16,21 @@ const SideBar = ({location}) => {
     const [isChangePasswordDialogueShown, setIsChangePasswordDialogueShown] = useState(false);
     const dispatch = useDispatch();
 
-    const handleClick = (event) => {
-        switch (event.currentTarget.className) {
-            case "userMenuButton": {
-                setIsUserMenuShown(!isUserMenuShown);
-                break
-            }
-            case "userMenuEntry": {
-                if (event.currentTarget.innerText === "Change password") {
-                    setIsChangePasswordDialogueShown(true);
-                    break
-                } else {
-                    dispatch({
-                        type: "auth/logout",
-                        payload: true
-                    });
-                    break
-                }
-            }
+    const handleUserMenuButtonClick = (event) => {
+        setIsUserMenuShown(!isUserMenuShown);
+    };
+
+    const handleUserMenuEntryClick = (event) => {
+        if (event.currentTarget.innerText === "Change password") {
+            setIsChangePasswordDialogueShown(true);
+        } else {
+            dispatch({
+                type: "auth/logout",
+                payload: true
+            });
         }
     };
+
 
     const hideUserMenu = () => {
         setIsUserMenuShown(false);
@@ -50,17 +45,18 @@ const SideBar = ({location}) => {
 
     return (
         <div className="d-flex flex-column h-100 justify-content-between sideBar mr-0 pr-0 pl-2">
-            <h4>PS</h4>
+            <h4 style={{ color: "#CFC0D3" }} >PS</h4>
             <div>
                 {
                     // check if there is a user name  to show
                     location && location.state && location.state.username
-                        ? <h4 className="userMenuButton"
-                              onClick={handleClick}>
+                        ? <h4 className="userMenuButton rotate-counter-clockwise"
+                              style={{ color: "#CFC0D3" }}
+                              onClick={handleUserMenuButtonClick}>
                             {location.state.username}
                             <img id="arrowDownIcon" src={arrowDownIcon} alt="arrow down" />
                         </h4>
-                        : <h4>Log in</h4>
+                        : <h4 className="rotate-counter-clockwise" style={{ color: "#CFC0D3" }}>Log in</h4>
                 }
             </div>
 
@@ -68,11 +64,11 @@ const SideBar = ({location}) => {
             {
                 isUserMenuShown
                 ? <div className="userMenu">
-                    <div onClick={handleClick} className="userMenuEntry">
+                    <div onClick={handleUserMenuEntryClick} className="userMenuEntry">
                         Change password
                         <img className="menuIcon" src={changePasswordIcon} alt="change password" />
                     </div>
-                    <div onClick={handleClick} className="userMenuEntry">
+                    <div onClick={handleUserMenuEntryClick} className="userMenuEntry">
                         Logout
                         <img className="menuIcon" src={logoutIcon} alt="logout" />
                     </div>
