62712ffdafc56868e1b321a1ecd7c36ffe96454d
ischekoldin <lesterfrance@gmail.com>
2020-09-05 00:10:49 +0500

    adjusted media queries for mobile devices

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -49,9 +49,33 @@ const SideBar = ({location}) => {
     return (
         <div className="row sideBar justify-content-between no-gutters">
 
-            <SideBarBody isMobile={isMobile}
-                         handleUserMenuButtonClick={handleUserMenuButtonClick}
-                         location={location} />
+            {/*<SideBarBody isMobile={isMobile}*/}
+            {/*             handleUserMenuButtonClick={handleUserMenuButtonClick}*/}
+            {/*             location={location} />*/}
+
+            <div className="col-sm-2 col-md-12 mt-md-5 pl-md-0 logo-col ">
+                <h4 id="logo" className="pl-sm-3 pl-md-0 mt-md-4 sideBarItem">UnNotate</h4>
+            </div>
+
+            <div className="col-sm-2 col-md-12 mt-md-auto user-menu-col">
+
+                {
+                    // check if there is a user name  to show
+                    location && location.state && location.state.username
+                        ?   <h4 className="userMenuButton sideBarItem"
+                                onClick={handleUserMenuButtonClick}>
+                                        <span className="text-nowrap">
+                                            {location.state.username}
+                                        </span>
+                            <i className="far fa-angle-down" />
+                        </h4>
+
+                        :   <h4 className="sideBarItem"><span className="text-nowrap">Log in</span></h4>
+                }
+
+            </div>
+
+
 
             {
                 isUserMenuShown
